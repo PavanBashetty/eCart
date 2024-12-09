1e67a328822bd28cfd13b93b3cb4624026d87022
PavanBashetty <pavankumar.rb25@gmail.com>
2024-12-09 04:44:55 +0100

    response entity included

--- src/main/java/com/dragonball/cartProject/controller/ProductController.java
+++ src/main/java/com/dragonball/cartProject/controller/ProductController.java
@@ -4,10 +4,9 @@ package com.dragonball.cartProject.controller;
 import com.dragonball.cartProject.model.Product;
 import com.dragonball.cartProject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +24,18 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/products/{prodId}")
+    public ResponseEntity<Product> getProductById(@PathVariable  int prodId){
+
+        Product product = service.getProductById(prodId);
+        if(product != null){
+            return new ResponseEntity<>(product,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/dragonball/cartProject/model/Product.java
+++ src/main/java/com/dragonball/cartProject/model/Product.java
@@ -1,13 +1,10 @@
 package com.dragonball.cartProject.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -22,6 +19,8 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private boolean available;
     private int quantity;
